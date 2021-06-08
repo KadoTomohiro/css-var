d04ba8d11dfcdec90ced2af855ce3fb2b80d31b5
KadoTomohiro <>
2021-06-08 09:40:16 +0900

    css property

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import css from  './assets/main.css'
 Vue.config.productionTip = false
 
 new Vue({
+  css,
   render: h => h(App),
 }).$mount('#app')
